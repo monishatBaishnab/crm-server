a312c473ee3f8e62b4b26db4877f68db8921a8ff
Monishat Baishnab <baishnabmonishat@gmail.com>
2025-04-23 23:38:15 +0600

    Add essential middlewares, utility helpers, and root routes.

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 import express, { Application } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import httpStatus from "http-status";
+import global_error from "./app/middlewares/global_error";
+import not_found from "./app/middlewares/not_found";
+import send_response from "./app/utils/send_response";
+import catch_async from "./app/utils/catch_async";
 
 // Create an instance of the Express application
 const app: Application = express();
@@ -17,5 +22,23 @@ app.use(
     origin: ["http://localhost:5173"],
   }),
 );
+
+// Define a GET route for the root URL
+app.get(
+  "/",
+  catch_async((req, res) => {
+    send_response(res, {
+      success: true,
+      status: httpStatus.OK,
+      message: "CRM Server Running Smoothly.",
+    });
+  }),
+);
+
+// Middleware to handle 404 (Not Found) errors
+app.use(/.*/, not_found);
+
+// Middleware to handle global errors
+app.use(global_error);
 
 export default app;
